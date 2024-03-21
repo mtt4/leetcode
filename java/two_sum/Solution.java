d5ff50dcf14b6aaf596794949c4da3e1710bc95a
Naota <152177623+mtt4@users.noreply.github.com>
2024-03-20 21:47:23 -0300

    Solved two-sum algorithm in Java

---
+++ java/two_sum/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j=i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+                // vvvv search this up later vvvv
+                if(nums[nums.length-i-1] + nums[nums.length-j-1] == target) {
+                    return new int[]{nums.length-j-1, nums.length-i-1};
+                }
+            }
+        }
+
+        return null;
+    }
+}
